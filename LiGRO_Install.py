de17ad79aab1d281dbb745df372c40c856bccb89
lkagami <luciano.kagami@ufrgs.br>
2017-01-23 09:35:05 -0200

    Update LiGRO_Install.py

--- LiGRO_Install.py
+++ LiGRO_Install.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python
 """
----LiGRO - Trial version---
+---LiGRO - version 0.1---
 
-Luciano Porto Kagami
-Universidade Federal do Rio Grande do Sul
-Laboratorio de Sintese Organica Medicinal -LaSOM
-Av. Ipiranga, 2752 - Azenha, Porto Alegre - RS, 90610-000 - Brazil
+This software is available to you under the terms of the GPL-3. See ~/ligro/LICENCE for more informations.
+               Software is created and maintained by Laboratorio de Sintese Organica Medicinal-LaSOM at
+               Universidade Federal do Rio Grande do Sul.
+               Contributors:
+               Luciano Porto Kagami
+               luciano_dot_kagami_at_ufrgs_dot_br
+               Av. Ipiranga, 2752 - Azenha, Porto Alegre - RS, 90610-000 - Brazil
+
+               For Antechamber, please cite:
+
+               1.  Wang, J., Wang, W., Kollman P. A.; Case, D. A. "Automatic atom type and
+               bond type perception in molecular mechanical calculations". Journal of
+               Molecular Graphics and Modelling , 25, 2006, 247260.
+               2.  Wang, J., Wolf, R. M.; Caldwell, J. W.; Kollman, P. A.; Case, D. A.
+               "Development and testing of a general AMBER force field". Journal of
+               Computational Chemistry, 25, 2004, 1157-1174.
+
+               For ACPYPE, please cite:
+
+               SOUSA DA SILVA, A. W.; VRANKEN, W. F.; LAUE, E. D. ACPYPE - AnteChamber
+               PYthon Parser Interface. Manuscript to be submitted.
+
+               Alan Wilter S. da Silva, D.Sc. - CCPN Research Associate
+               Department of Biochemistry, University of Cambridge.
+               80 Tennis Court Road, Cambridge CB2 1GA, UK.
+               >>http://www.bio.cam.ac.uk/~awd28<<
+
+               alanwilter _at_ gmail _dot_ com
+
+               For PLIP please cite:
+
+               Salentin,S. et al. PLIP: fully automated protein-
+               ligand interaction profiler. Nucl. Acids Res. (1 July 2015) 43 (W1):
+               W443-W447. doi: 10.1093/nar/gkv315
+
+               For GROMACS please cite:
+
+               For this code plese cite:
+               Manuscript to be submitted.
 
 -------------------
 """
@@ -262,7 +297,7 @@ if __name__ == '__main__':
     scrollbar.config(command=window_txt.yview)
 
     my_opening_message0 = """
-               This software is available to you under the terms of the GPL-3.
+               This software is available to you under the terms of the GPL-3. See ~/ligro/LICENCE for more informations.
                Software is created and maintained by Laboratorio de Sintese Organica Medicinal-LaSOM at
                Universidade Federal do Rio Grande do Sul.
                Contributors:
