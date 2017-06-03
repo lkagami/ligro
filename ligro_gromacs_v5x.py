f56fca7b0629d874fc442b71703d3559be01bc28
lkagami <luciano.kagami@ufrgs.br>
2017-06-03 08:58:09 -0300

    Update ligro_gromacs_v5x.py

--- ligro_gromacs_v5x.py
+++ ligro_gromacs_v5x.py
@@ -342,27 +342,28 @@ class GUI:
         cmd1 = 'gmx editconf -bt {0} -f complex.pdb -o trpb4solv.pdb -d {1}'.format(bx, dst)
         os.system(cmd1)
         os.system('gmx solvate -cp trpb4solv.pdb -cs spc216.gro -o trpb4ion.pdb -p trp.top')
-        os.system('''cat << EOF >| em.mdp
-          ; LINES STARTING WITH ';' ARE COMMENTS
-	  title		= Minimization	; Title of run
+        os.system('''
+		cat << EOF >| em.mdp
+          	; LINES STARTING WITH ';' ARE COMMENTS
+	  	title		= Minimization	; Title of run
 
-	  ; Parameters describing what to do, when to stop and what to save
-	  integrator	= steep		; Algorithm (steep = steepest descent minimization)
-	  emtol		= 1000.0  	; Stop minimization when the maximum force < 10.0 kJ/mol
-	  emstep      = 0.01      ; Energy step size
-	  nsteps		= 50000	  	; Maximum number of (minimization) steps to perform
-	  energygrps	= system	; Which energy group(s) to write to disk
+	  	; Parameters describing what to do, when to stop and what to save
+	  	integrator	= steep		; Algorithm (steep = steepest descent minimization)
+	  	emtol		= 1000.0  	; Stop minimization when the maximum force < 10.0 kJ/mol
+	  	emstep      = 0.01      ; Energy step size
+	  	nsteps		= 50000	  	; Maximum number of (minimization) steps to perform
+		energygrps	= system	; Which energy group(s) to write to disk
 
-	  ; Parameters describing how to find the neighbors of each atom and how to calculate the interactions
-	  nstlist		    = 1		    ; Frequency to update the neighbor list and long range forces
-	  cutoff-scheme   = Verlet
-	  ns_type		    = grid		; Method to determine neighbor list (simple, grid)
-	  rlist		    = 1.0		; Cut-off for making neighbor list (short range forces)
-	  coulombtype	    = PME		; Treatment of long range electrostatic interactions
-	  rcoulomb	    = 1.0		; long range electrostatic cut-off
-	  rvdw		    = 1.0		; long range Van der Waals cut-off
-	  pbc             = xyz 		; Periodic Boundary Conditions
-	  EOF
+	  	; Parameters describing how to find the neighbors of each atom and how to calculate the interactions
+	  	nstlist		    = 1		    ; Frequency to update the neighbor list and long range forces
+	  	cutoff-scheme   = Verlet
+	  	ns_type		    = grid		; Method to determine neighbor list (simple, grid)
+	  	rlist		    = 1.0		; Cut-off for making neighbor list (short range forces)
+	  	coulombtype	    = PME		; Treatment of long range electrostatic interactions
+	  	rcoulomb	    = 1.0		; long range electrostatic cut-off
+	  	rvdw		    = 1.0		; long range Van der Waals cut-off
+	 	pbc             = xyz 		; Periodic Boundary Conditions
+	  	EOF
                     ''')
 
                  
