b30d6eec7b8c4554348532577262a5167a9d0476
lkagami <luciano.kagami@ufrgs.br>
2018-11-10 08:53:53 -0200

    Update setup.py

--- recipe/setup.py
+++ recipe/setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages, Extension
 setup(
     name='ligro',
     packages=['ligro'],
-    version='0.3',
+    version='0.6',
     description='LiGRO: a graphical user interface for protein-ligand molecular dynamics.',
     author="KAGAMI, Luciano Porto",
     author_email='luciano.kagami@ufrgs.br',
     url='https://github.com/lkagami/ligro',
     keywords=['ligro'],
     include_package_data=True,
-    entry_points={'console_scripts': ['ligro = ligro_gromacs:main_init']},
+    entry_points={'console_scripts': ['ligro = ligro.ligro_gromacs:main_init']},
     zip_safe=False,
     classifiers=[
         '',
